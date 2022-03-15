4b649a89647358b29765e1d2cd8b921396236fc7
TingyuZhu <100369383+TingyuZhu@users.noreply.github.com>
2022-03-15 11:16:01 +0000

    Add files via upload

---
+++ BSmag_add_filament.m
@@ -0,0 +1,40 @@
+function [BSmag] = BSmag_add_filament(BSmag,Gamma,I,dGamma)
+%---------------------------------------------------
+%  NAME:      BSmag_add_filament.m
+%  WHAT:      Adds a filament to the BSmag analysis.
+%  REQUIRED:  BSmag Toolbox 20150407
+%  AUTHOR:    20150407, L. Queval (loic.queval@gmail.com)
+%  COPYRIGHT: 2015, Loic Queval, BSD License (http://opensource.org/licenses/BSD-3-Clause).
+%
+%  USE:
+%  [BSmag] = BSmag_add_filament(BSmag,Gamma,I,dGamma)
+%
+%  INPUTS:
+%    BSmag      = BSmag data structure
+%    Gamma      = Filament points coordinates (x,y,z), one point per line [m,m,m]
+%    I          = Filament current (flows from first point towards last point) [A]
+%    dGamma     = Filament max discretization step [m]
+%
+%  OUTPUTS:
+%    BSmag      = Updated BSmag data structure
+%      BSmag.Nfilament              = Number of filaments
+%      BSmag.filament(*).*          = Filament structure
+%      BSmag.filament(*).Gamma      = Filament points coordinates (x,y,z), one point per line [m,m,m]
+%      BSmag.filament(*).I          = Filament current (flows from first point towards last point) [A]
+%      BSmag.filament(*).dGamma     = Filament max discretization step [m]
+%----------------------------------------------------
+
+n = BSmag.Nfilament+1;
+BSmag.filament(n).Gamma = [0, 0, 0;
+    0, 0, 2;
+    0, 4.2, 2;
+    0, 4.2, 0;
+    0, 0, 0] - [0, 2.1, 1]; 
+BSmag.filament(n).I = 0.03; %[A]
+BSmag.filament(n).dGamma = 1e-3; % filament max discretization step [m] 
+BSmag.Nfilament = n;
+
+%Plot P (where there is a current source)
+figure(1)
+	plot3(Gamma(:,1),Gamma(:,2),Gamma(:,3),'.-r')
+axis tight
